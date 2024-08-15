67248e95a931b0a6c83f27afc039a542a62d9a23
aburke1605 <aodhanburke@hotmail.com>
2024-08-15 20:19:33 +0100

    add some randomness to generate mines instead of squares

--- Grid.cpp
+++ Grid.cpp
@@ -1,4 +1,5 @@
 #include "Point.cpp"
+#include <random>
 
 class Grid {
 public:
@@ -6,12 +7,17 @@ public:
 		sf::Vector2f size(30.0f, 20.0f);
 		float width = window.getSize().x / size.x;
 
+		_generator = std::mt19937(_rd()); // mersenne_twister_engine seeded with rd()
+		std::uniform_int_distribution<> distribution(0, 4);
 
 		for (int i = 0; i < (int)size.x; i++) {
 			std::vector<Point*> row;
 			for (int j = 0; j < (int)size.y; j++) {
 				sf::Vector2f position(float(i) * window.getSize().x / size.x, float(j) * window.getSize().y / size.y);
-				row.push_back(new Square(position, width));
+				if (distribution(_generator) == 0) // 20% of the squares are mines
+					row.push_back(new Mine(position, width));
+				else
+					row.push_back(new Square(position, width));
 			}
 			_rows_cols.push_back(row);
 		}
@@ -32,4 +38,7 @@ public:
 
 private:
 	std::vector<std::vector<Point*>> _rows_cols;
+
+	std::random_device _rd; // a seed source for the random number engine
+	std::mt19937 _generator;
 };
