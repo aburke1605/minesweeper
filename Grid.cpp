df86796adb350074b66e8f3b3dd0b5077b0e1abf
aburke1605 <aodhanburke@hotmail.com>
2024-08-15 20:21:12 +0100

    more readable

--- Grid.cpp
+++ Grid.cpp
@@ -2,43 +2,43 @@
 #include <random>
 
 class Grid {
-public:
-	Grid(sf::RenderWindow& window) {
-		sf::Vector2f size(30.0f, 20.0f);
-		float width = window.getSize().x / size.x;
+	public:
+		Grid(sf::RenderWindow& window) {
+			sf::Vector2f size(30.0f, 20.0f);
+			float width = window.getSize().x / size.x;
 
-		_generator = std::mt19937(_rd()); // mersenne_twister_engine seeded with rd()
-		std::uniform_int_distribution<> distribution(0, 4);
+			_generator = std::mt19937(_rd()); // mersenne_twister_engine seeded with rd()
+			std::uniform_int_distribution<> distribution(0, 4);
 
-		for (int i = 0; i < (int)size.x; i++) {
-			std::vector<Point*> row;
-			for (int j = 0; j < (int)size.y; j++) {
-				sf::Vector2f position(float(i) * window.getSize().x / size.x, float(j) * window.getSize().y / size.y);
-				if (distribution(_generator) == 0) // 20% of the squares are mines
-					row.push_back(new Mine(position, width));
-				else
-					row.push_back(new Square(position, width));
-			}
-			_rows_cols.push_back(row);
-		}
-	}
-
-	std::vector<std::vector<Point*>>& GetPoints() {
-		return _rows_cols;
-	}
-
-	void Draw(sf::RenderWindow& window) {
-		for (auto& row : _rows_cols) {
-			for (auto& point : row) {
-				if (point != nullptr)
-					point->Draw(window);
+			for (int i = 0; i < (int)size.x; i++) {
+				std::vector<Point*> row;
+				for (int j = 0; j < (int)size.y; j++) {
+					sf::Vector2f position(float(i) * window.getSize().x / size.x, float(j) * window.getSize().y / size.y);
+					if (distribution(_generator) == 0) // 20% of the squares are mines
+						row.push_back(new Mine(position, width));
+					else
+						row.push_back(new Square(position, width));
+				}
+				_rows_cols.push_back(row);
 			}
 		}
-	}
 
-private:
-	std::vector<std::vector<Point*>> _rows_cols;
+		std::vector<std::vector<Point*>>& GetPoints() {
+			return _rows_cols;
+		}
 
-	std::random_device _rd; // a seed source for the random number engine
-	std::mt19937 _generator;
+		void Draw(sf::RenderWindow& window) {
+			for (auto& row : _rows_cols) {
+				for (auto& point : row) {
+					if (point != nullptr)
+						point->Draw(window);
+				}
+			}
+		}
+
+	private:
+		std::vector<std::vector<Point*>> _rows_cols;
+
+		std::random_device _rd; // a seed source for the random number engine
+		std::mt19937 _generator;
 };
--- Point.cpp
+++ Point.cpp
@@ -2,44 +2,48 @@
 #include <SFML/Graphics.hpp>
 
 class Point {
-public:
-	Point(sf::Vector2f position, float size) {
-		_position = position;
-		_edges = std::make_pair(position, position + sf::Vector2f(size, size));
-	}
-	sf::Vector2f GetPosition() const {
-		return _position;
-	}
-	std::pair< sf::Vector2f, sf::Vector2f> GetEdges() const {
-		return _edges;
-	}
-	virtual void Draw(sf::RenderWindow& window) const {}
-private:
-	sf::Vector2f _position;
-	std::pair<sf::Vector2f, sf::Vector2f> _edges;
+	public:
+		Point(sf::Vector2f position, float size) {
+			_position = position;
+			_edges = std::make_pair(position, position + sf::Vector2f(size, size));
+		}
+		sf::Vector2f GetPosition() const {
+			return _position;
+		}
+		std::pair< sf::Vector2f, sf::Vector2f> GetEdges() const {
+			return _edges;
+		}
+		virtual void Draw(sf::RenderWindow& window) const {}
+	private:
+		sf::Vector2f _position;
+		std::pair<sf::Vector2f, sf::Vector2f> _edges;
 };
+
+
 
 class Square : public Point {
-public:
-	Square(sf::Vector2f position, float size) : Point(position, size) {
-		_rect = sf::RectangleShape(sf::Vector2f(19.0f, 19.0f));
-		_rect.setPosition(GetPosition());
-		_flagged = false;
-		_n_mines_in_proximity = 0;
-	}
-	void Draw(sf::RenderWindow& window) const {
-		window.draw(_rect);
-	}
-private:
-	sf::RectangleShape _rect;
-	bool _flagged;
-	unsigned int _n_mines_in_proximity;
+	public:
+		Square(sf::Vector2f position, float size) : Point(position, size) {
+			_rect = sf::RectangleShape(sf::Vector2f(19.0f, 19.0f));
+			_rect.setPosition(GetPosition());
+			_flagged = false;
+			_n_mines_in_proximity = 0;
+		}
+		void Draw(sf::RenderWindow& window) const {
+			window.draw(_rect);
+		}
+	private:
+		sf::RectangleShape _rect;
+		bool _flagged;
+		unsigned int _n_mines_in_proximity;
 };
 
-class Mine : public Point {
-public:
-	Mine(sf::Vector2f position, float size) : Point(position, size) {}
-	void Draw() const {
 
-	}
+
+class Mine : public Point {
+	public:
+		Mine(sf::Vector2f position, float size) : Point(position, size) {}
+		void Draw() const {
+
+		}
 };
